1fc0c9bf4f49d9fcf1369f9478051d2df5de9520
ambiti0n <fabulousfresh010@gmail.com>
2023-03-23 01:22:37 +0100

    react query for fetching and catching

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export default function App({ Component, pageProps }) {
+  const [queryClient] = React.useState(() => new QueryClient());
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
