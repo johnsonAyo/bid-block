a0081708b075ba1596997149343b1ad9f0547234
ambiti0n <fabulousfresh010@gmail.com>
2023-03-28 04:00:32 +0100

    chore: nft explore page

---
+++ components/homepage/HomePage.js
@@ -0,0 +1,113 @@
+import { Box, Text, Grid, Button, Input, Flex } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+
+export default function HomePage() {
+  const basicBoxStyles = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    width: "100vw",
+    height: "90vh",
+    color: "white",
+    textShadow: "0 0 20px black",
+    fontWeight: "bold",
+    fontSize: "20px",
+    px: 4,
+    background: "url('/images/svg/plain_bg.svg') center/cover no-repeat",
+  };
+
+  return (
+    <Box sx={basicBoxStyles}>
+      <Flex justifyContent="space-between" width="40%">
+        <Input
+          placeholder="Search"
+          htmlSize={4}
+          width="sm"
+          bgColor="white"
+          variant="filled"
+        />
+        <Button
+          colorScheme="black"
+          size="md"
+          paddingX={10}
+          paddingY={5}
+          color="white"
+          variant="outline"
+          border="2px"
+          borderColor="white"
+        >
+          Create
+        </Button>
+      </Flex>
+      <Box height="85%" width="80%">
+        <Flex justifyContent="space-between" width="100%" padding="1rem">
+          <Text fontSize="3xl"> Live Auction</Text>
+          <Text fontSize="lg"> See All</Text>
+        </Flex>
+        <Flex
+          height="85%"
+          variant="outline"
+          border="2px"
+          borderColor="white"
+          bg="black"
+        >
+          <Flex
+            gap={3}
+            flexDirection="Column"
+            width="55%"
+            justifyContent="start"
+            alignItems="start"
+            padding={10}
+          >
+            <Box display="flex">
+              <a href="./nft-auction-page">
+                <Image boxSize="100%" src="/images/svg/profile.svg" alt="" />
+              </a>
+              <Box marginLeft={5}>
+                <Text marginBottom={2} fontSize="md">
+                  @TheManKinniboss
+                </Text>
+                <Text fontSize="xs"> Created on 23-12-2023</Text>
+              </Box>
+            </Box>
+            <Text fontSize="md">Starting Price</Text>
+            <Text fontSize="3xl">0.45 Eth</Text>
+            <Flex gap={10}>
+              <Text fontSize="sm">Auction Date</Text>
+              <Text fontSize="sm">Time</Text>
+            </Flex>
+            <Flex gap={5}>
+              <Text fontSize="lg">21-12-2022</Text>
+              <Text fontSize="lg">12:00 GMT</Text>
+              <Text fontSize="sm">Auction runs for 24Hrs</Text>
+            </Flex>
+            <Text fontSize="lg">CountDown</Text>
+            <Text fontSize="3xl">22:43:00</Text>
+            <Button
+              colorScheme="black"
+              size="xlg"
+              paddingX={20}
+              paddingY={5}
+              color="white"
+              variant="outline"
+              border="2px"
+              borderColor="white"
+              marginLeft="200px"
+              marginTop={5}
+            >
+              Enter Auction
+            </Button>
+          </Flex>
+
+          <Box>
+            <a href="./nft-auction-page">
+              <Image boxSize="100%" src="/images/svg/NFT1.svg" alt="" />
+            </a>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+}
