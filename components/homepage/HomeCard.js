fb948682a5f5c198bce6c5b8ae9091e7d8e79d10
ambiti0n <fabulousfresh010@gmail.com>
2023-03-28 01:17:22 +0100

    chore: homepage built

---
+++ components/homepage/HomeCard.js
@@ -0,0 +1,43 @@
+import { Box, Text, Grid, Button } from "@chakra-ui/react";
+import { Black_And_White_Picture } from "next/font/google";
+
+export default function HomeCard() {
+  const basicBoxStyles = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    width: "100vw",
+    height: "100vh",
+    color: "white",
+    textShadow: "0 0 20px black",
+    fontWeight: "bold",
+    fontSize: "20px",
+    px: 4,
+    background: "url('/images/svg/bg_home.svg') center/cover no-repeat",
+  };
+
+  return (
+    <Box sx={basicBoxStyles}>
+      <Box marginTop={20}>
+        <Text fontSize="5xl" fontWeight="extrabold">
+          Welcome to the Future of Autions <br />
+          where Heroes are made and legends are born
+        </Text>
+        <Button
+          colorScheme="blue"
+          size="xlg"
+          paddingX={10}
+          paddingY={5}
+          marginTop={20}
+          color="white"
+          variant="outline"
+          border="2px"
+          borderColor="white"
+        >
+          Get Started
+        </Button>
+      </Box>
+    </Box>
+  );
+}
